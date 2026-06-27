12cbcb126f23dc3e51b0f09f669cc5860b4f46e4
sadithsenanayake-blip <sadithsenanayake@gmail.com>
2026-06-27 21:02:47 +0530

    Updated Projectss Section

--- script.js
+++ script.js
@@ -89,6 +89,42 @@ skillCards.forEach((card) => {
     skillObserver.observe(card);
 });
 
+const filterButtons =
+document.querySelectorAll(".filter-btn");
+
+const projectCards =
+document.querySelectorAll(".project-card");
+
+filterButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        filterButtons.forEach(btn =>
+            btn.classList.remove("active")
+        );
+
+        button.classList.add("active");
+
+        const filter =
+            button.dataset.filter;
+
+        projectCards.forEach(card => {
+
+            if (
+                filter === "all" ||
+                card.dataset.category === filter
+            ) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
+
 // Scroll Animation
 const sections = document.querySelectorAll(".profile-card, .edu-card, .skill, .project-card, .contact-Box");
 
@@ -105,4 +141,3 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((item) => {
     observer.observe(item);
 });
-
