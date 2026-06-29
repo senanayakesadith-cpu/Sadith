959db3cf66f0c9857f3bb4ce006aa09da691fabd
sadithsenanayake-blip <sadithsenanayake@gmail.com>
2026-06-29 10:27:32 +0530

    Updated Mobile Version

--- script.js
+++ script.js
@@ -243,3 +243,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
